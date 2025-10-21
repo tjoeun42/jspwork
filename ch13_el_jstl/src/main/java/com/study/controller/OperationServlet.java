544e353a2e9bdff3534cf23c32160c596319d594
tjoeun42 <tjoeunit@gmail.com>
2025-10-21 17:50:43 +0900

    11일차_3교시_jstl

--- ch13_el_jstl/src/main/java/com/study/controller/OperationServlet.java
+++ ch13_el_jstl/src/main/java/com/study/controller/OperationServlet.java
@@ -1,11 +1,15 @@
 package com.study.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.study.dto.Person;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/operation.do")
 public class OperationServlet extends HttpServlet {
@@ -14,6 +18,20 @@ public class OperationServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setAttribute("big", 10);
 		request.setAttribute("small", 3);
+		
+		request.setAttribute("sOne", "");
+		request.setAttribute("sTwo", "");
+		request.setAttribute("sThree", "안녕");
+		
+		request.setAttribute("pOne", new Person("", 20, ""));
+		request.setAttribute("pTwo", null);
+		
+		ArrayList<String> list1 = new ArrayList<>();
+		request.setAttribute("aOne", list1);
+		
+		ArrayList<String> list2 = new ArrayList<>();
+		list2.add("더조은컴퓨터아카데미");
+		request.setAttribute("aTwo", list2);
 		
 		request.getRequestDispatcher("views/1_EL/02.elOperation.jsp").forward(request, response);
 	}
---
+++ ch13_el_jstl/src/main/webapp/views/2_coustomAction/jstl.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>jstl</title>
+</head>
+<body>
+	<h1>JSTL이란?</h1>
+	<p>
+		JSP Standard Tag Library의 약자로 jsp에서 사용되는 커스텀 액션 태그로<br>
+		공통적으로 자주 사용되는 코드들을 보다 쉽게 사용할 수 있도록 태그화하여 표준으로 제공하는 라이브러리<br>
+		필요한 라이브러리를 추가 한후 사용가능
+	</p>
+</body>
+</html>
+
+
+
+
+
+
